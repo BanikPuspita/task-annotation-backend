d7a1c43c902857b341af6fe465b96acd9497db65
puspita <puspitabanik213@gmail.com>
2026-07-11 23:22:02 +0600

    Create default admin on deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User;
+u='admin';
+e='admin@example.com';
+p='admin123';
+if not User.objects.filter(username=u).exists():
+    User.objects.create_superuser(u, e, p)
+"
