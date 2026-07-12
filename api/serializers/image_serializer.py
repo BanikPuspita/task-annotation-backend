487f7f94f2dd5cac040c7d145bd25ed76d486d1c
puspita <puspitabanik213@gmail.com>
2026-07-12 12:48:47 +0600

    new update

--- api/serializers/image_serializer.py
+++ api/serializers/image_serializer.py
@@ -3,7 +3,17 @@ from api.models import Image
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
         fields = "__all__"
         read_only_fields = ["user"]
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+
+        if request:
+            return request.build_absolute_uri(obj.image.url)
+
+        return obj.image.url
